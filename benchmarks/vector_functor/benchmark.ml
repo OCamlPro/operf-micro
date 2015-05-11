8a0c498b0abe033c1fb0440ba62139c3b5acab4e
Pierre Chambart <pierre.chambart@ocamlpro.com>
2015-05-11 18:58:20 +0200

    Add a few more vector benchmarks

--- benchmarks/vector_functor/benchmark.ml
+++ benchmarks/vector_functor/benchmark.ml
@@ -12,6 +12,34 @@ let () =
       (fun i -> { VR2.x = float i; VR2.y = float (i + 1) }),
       (fun i res -> (float i) *. (float i) +. (float (i+1)) *. (float (i+1)) =!= res),
       [ Any, Short ]);
+    "vec2 tuple dot product",
+    Int (
+      (fun x -> IV2.dot x x),
+      (fun i -> (float i, float (i + 1))),
+      (fun i res -> (float i) *. (float i) +. (float (i+1)) *. (float (i+1)) =!= res),
+      [ Any, Short ]);
+    "vec3 tuple dot product",
+    Int (
+      (fun x -> IV3.dot x x),
+      (fun i -> (float i, float (i + 1), float (i + 2))),
+      (fun i res ->
+         (float i) *. (float i) +. (float (i+1)) *. (float (i+1))
+         +. (float (i+2)) *. (float (i+2)) =!= res),
+      [ Any, Short ]);
+    "vec3 record dot product",
+    Int (
+      (fun x -> IV3R.dot x x),
+      (fun i -> { x = float i; y = float (i + 1); z = float (i + 2) }),
+      (fun i res ->
+         (float i) *. (float i) +. (float (i+1)) *. (float (i+1))
+         +. (float (i+2)) *. (float (i+2)) =!= res),
+      [ Any, Short ]);
+    "nothing",
+    Int (
+      (fun x -> ()),
+      (fun i -> ()),
+      (fun i res -> Ok),
+      [ Any, Short ])
   ]
 
 (* TODO add the other ones *)
